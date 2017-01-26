2335566e4638a03a9fcdf3720c624c7b78872e64
finnhodgkin <finnhodgkin@gmail.com>
2017-01-26 17:37:53 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,86 @@
+function applyAnchor(tag) {
+    var elements = document.getElementsByTagName(tag);
+    var anchorName = null;
+    var link = null;
+    var div = null;
+    var span = null;
+    for (var i = 0; i < elements.length; i++) {
+      anchorName = elements[i].innerHTML.replace(/ /g, '-');
+      div = document.createElement('div');
+            div.id = anchorName;
+            div.classList = 'anchor';
+      span = document.createElement('span');
+             span.appendChild(document.createTextNode(' '));
+             span.classList = 'anchor-span';
+      link = document.createElement('a');
+             link.appendChild(document.createTextNode('#'));
+             link.href = '#' + anchorName;
+      span.appendChild(link);
+      elements[i].appendChild(span);
+      elements[i].insertBefore(div, elements[i].firstChild);
+    }
+}
+
+function buildContents(element) {
+    applyAnchor('h1');
+    applyAnchor('h2');
+
+    newBuildList(document.getElementsByClassName('anchor'), document.getElementById(element));
+}
+
+function newBuildList(list, element) {
+  var prev = null;
+  var ul = document.createElement('ul');
+  var header = document.createElement('h3');
+  header.appendChild(document.createTextNode('Page contents'));
+  element.appendChild(header);
+  element.appendChild(ul); // Base list
+  for (var i = 0; i < list.length; i++) {
+    var link = document.createElement('a');
+    var linkText = list[i].id.replace(/-/g, ' ');
+    var currentTag = list[i].parentElement.nodeName;
+    link.href = i === 0 ? '#' : '#' + list[i].id;
+    if (currentTag === 'H1' || i === 0) {
+      element.lastChild
+             .appendChild(document.createElement('li'))
+             .appendChild(link)
+             .appendChild(document.createTextNode(linkText));
+    } else { // H2
+      if (prev === 'H1' || i === 1) {
+        element.lastChild
+               .appendChild(document.createElement('ul'))
+               .appendChild(document.createElement('li'))
+               .appendChild(link)
+               .appendChild(document.createTextNode(linkText));
+      } else {
+        element.lastChild.lastChild
+               .appendChild(document.createElement('li'))
+               .appendChild(link)
+               .appendChild(document.createTextNode(linkText));
+      }
+    }
+    prev = currentTag;
+  }
+}
+
+buildContents('tableOfContents');
+
+(function navbar(nav) {
+  var item = document.getElementsByTagName(nav)[0]; // first nav in doc
+  var height = document.getElementById('headercontainer').clientHeight;
+  function scroll(loc) {
+    if (loc > height) {
+      console.log(item.classList);
+      if (!item.classList.contains('nav__fixed')) {
+        item.classList = 'nav__fixed';
+      }
+    } else {
+      if (item.classList.contains('nav__fixed')) {
+        item.classList = '';
+      }
+    }
+  }
+  window.onscroll = function (e) {
+    scroll(e.pageY);
+  };
+})('nav');
